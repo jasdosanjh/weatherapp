00d19c947af5003a64f4abe9d7ee733de64a7fcb
jazdosanjh <jas@dosanjh.co.uk>
2019-08-09 11:55:36 +0100

    Finished both functions to make both city information, and weather information requests from the accuweather API.

--- js/forecast.js
+++ js/forecast.js
@@ -1,5 +1,17 @@
 const key = 'XINwivSfXuQMf8H95LpjZtBoazaOOFNH';
 
+// get weather information
+const getWeather = async id => {
+  const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city information
 const getCity = async city => {
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
@@ -10,6 +22,11 @@ const getCity = async city => {
   return data[0];
 };
 
-getCity('nottingham')
-  .then(data => console.log(data))
+getCity('Nottingham')
+  .then(data => {
+    return getWeather(data.Key);
+  })
+  .then(data => {
+    console.log(data);
+  })
   .catch(err => console.log(err));
