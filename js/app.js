ad39d1d0d00e4c7f83ab65d7aa7a52dd176cc3c1
jazdosanjh <jas@dosanjh.co.uk>
2019-08-09 12:07:09 +0100

    Added event handler to the form so I can now search, and see results in the console by directly interacting with the form.

---
+++ js/app.js
@@ -0,0 +1,25 @@
+const cityForm = document.querySelector('form');
+
+const updateCity = async city => {
+  const cityDetails = await getCity(city);
+  const weather = await getWeather(cityDetails.Key);
+
+  return {
+    cityDetails: cityDetails,
+    weather: weather
+  };
+};
+
+cityForm.addEventListener('submit', e => {
+  // prevent default behaviour
+  e.preventDefault();
+
+  // get city value
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update ui with requested city
+  updateCity(city)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+});
